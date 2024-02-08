245c879c77a4fd1155ec70e49d49218da14819ae
Tyutin Kirill <kerya.tyutin@yandex.ru>
2024-02-08 21:02:59 +0400

    main page start

--- portfolio-frontend/src/app/(main)/page.tsx
+++ portfolio-frontend/src/app/(main)/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import "./MainPage.scss";
 
-export default function page() {
+export default function MainPage() {
   return (
-    <div>page</div>
-  )
+    <div className="main-page">
+      <section className="main-page__section">
+        <div className="main-page__section-info">
+          <h3 className="main-page__section-subtitle">Советы по обучению</h3>
+          <h2 className="main-page__section-title">Преподаватели</h2>
+          <p className="main-page__section-info-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+            senectus neque, lorem sit in mattis. Vehicula eget eget tellus{" "}
+          </p>
+        </div>
+      </section>
+
+      <section className="main-page__section">
+        <div className="main-page__section-info">
+          <h3 className="main-page__section-subtitle">Работы</h3>
+          <h2 className="main-page__section-title">Портфолио студентов</h2>
+          <p className="main-page__section-info-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+            senectus neque, lorem sit in mattis. Vehicula eget eget tellus{" "}
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
