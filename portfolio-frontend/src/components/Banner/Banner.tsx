20705fe141b9f03b64d03e93dfbe468bee15c334
Tyutin Kirill <kerya.tyutin@yandex.ru>
2024-02-13 22:38:44 +0400

    fix about

--- portfolio-frontend/src/components/Banner/Banner.tsx
+++ portfolio-frontend/src/components/Banner/Banner.tsx
@@ -7,7 +7,7 @@ export default function Banner() {
       <h1 className="banner__title">Онлайн портфолио кафедры ”Технология художественной обработки материалов”</h1>
       <div className="banner__links">
         <Button theme='transparent' tag='a' href='/works'>Работы</Button>
-        <Button theme='transparent' tag='a' href='/#about'>О нас</Button>
+        <Button theme='transparent' tag='a' href='/about'>О нас</Button>
       </div>
     </div>
   )
